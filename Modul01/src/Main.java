ecd943ea4879e2b89e0178973e1c78d9771bf72c
Sofyan Assauri <sofyanassauri.je@gmail.com>
2021-06-04 23:44:43 +0800

    Project IDEA

---
+++ Modul01/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Sofyan";
+        String namaBelakang = "Assauri";
+        int usia = 20;
+        int lamaKuliah = 4;
+        double ipk =3.89;
+        char nilai = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + " " + namaBelakang);
+    }
+}
