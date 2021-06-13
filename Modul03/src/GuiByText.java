fff93818b07bfae8d246f15aedd0fb25afe2a9e5
Sofyan Assauri <sofyanassauri.je@gmail.com>
2021-06-13 22:02:01 +0800

    menambahkan event listener

--- Modul03/src/GuiByText.java
+++ Modul03/src/GuiByText.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GuiByText extends JFrame {
 
     JLabel label1 = new JLabel("Coba Input 1");
     JTextField textField1 = new JTextField();
+    JButton button1 = new JButton();
 
     GuiByText(){
         setSize(640, 480);
@@ -20,10 +23,24 @@ public class GuiByText extends JFrame {
 
         label1.setBounds(16,32,100,25);
         textField1.setBounds(124,32,100,25);
+        button1.setBounds(232,32,80,25);
+    }
+
+    void menambahkanListener(){
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nama = textField1.getText();
+                JOptionPane.showMessageDialog(null, "Hai " + nama);
+            }
+        });
     }
 
     public static void main(String[] args) {
         GuiByText guiByText = new GuiByText();
+        guiByText.meletakkanKomponen();
+        guiByText.menambahkanListener();
         guiByText.setVisible(true);
     }
+
 }
